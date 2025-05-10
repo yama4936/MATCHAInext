35b73aab0a6bfbf014fb12b262bf906618bb7825
yama4936 <t323088@chukyo-u.ac.jp>
2025-05-10 20:41:39 +0900

    supabaseのドメインを登録

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "qcqbzyvveeufngsoijwe.supabase.co", // あなたのSupabaseドメイン
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
     ],
   },
 };
